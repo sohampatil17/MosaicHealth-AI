71f5319be3abafce0b19483f6136eb889bccfe76
kishtiijKaushal <kishitijk@vt.edu>
2024-03-23 19:09:40 -0400

    add logo

--- Mosaic-Health-AI-Web/src/components/Navbar.tsx
+++ Mosaic-Health-AI-Web/src/components/Navbar.tsx
@@ -1,10 +1,15 @@
-import { Card, Typography } from '@mui/joy';
+import { Card, Typography, Box } from '@mui/joy';
 
 export default function NavBar() {
 
     return (
         <Card color='primary' variant='soft' sx={{ margin: 0, height: 50, justifyContent: 'center' }}>
-            <Typography level='h3'>Mosaic Health</Typography>
-        </Card >
+            <Box sx={{ display: 'flex', alignItems: 'center', marginTop: '10px' }}>
+        <Typography level='h3'>Mosaic Health</Typography>
+        <img src="./src/assets/mosaic-icon.svg" alt="Logo" width="50" height="50" />
+    </Box>
+        </Card>
     );
 }
+            
+    
